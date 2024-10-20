986958d08838b5578aad510c5119aed00bc60c01
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-20 21:04:28 +0900

    Style | 버튼에 이미지 추가

--- src/components/search.rs
+++ src/components/search.rs
@@ -1,5 +1,5 @@
-use std::str::FromStr;
 use leptos::*;
+use std::str::FromStr;
 
 use crate::interfaces::filter::FILTER;
 use crate::interfaces::port::PortInfo;
@@ -13,7 +13,7 @@ pub fn SearchBar(
     // 'static + Clone을 명시적으로 지정하여, 이벤트 핸들러로 사용되는 클로저가 컴포넌트의 라이프타임 동안 안전하게 사용될 수 있도록 보장
     clear_event: impl Fn() + 'static + Clone,
 ) -> impl IntoView {
-    let (filter, set_filter) =create_signal(FILTER::PROCESS);
+    let (filter, set_filter) = create_signal(FILTER::PROCESS);
     let input_ref = create_node_ref::<html::Input>();
 
     let on_search_change = move |e| {
@@ -23,7 +23,10 @@ pub fn SearchBar(
         set_filter_ports.set(ports.get());
         set_filter_ports.update(|ports| {
             ports.retain(|port| match filter.get() {
-                FILTER::PROCESS => port.get_process_name().to_lowercase().contains(&value.to_lowercase()),
+                FILTER::PROCESS => port
+                    .get_process_name()
+                    .to_lowercase()
+                    .contains(&value.to_lowercase()),
                 FILTER::PORT => port.get_port().contains(&value),
                 FILTER::PID => port.get_pid().contains(&value),
             });
@@ -39,10 +42,10 @@ pub fn SearchBar(
                 placeholder=""
                 on:input=on_search_change
             />
-            <div class="flex flex-nowrap items-center px-2 rounded-lg space-x-4 mx-auto">
+            <div class="flex flex-nowrap items-center px-2 rounded-lg space-x-4 mx-auto h-full">
                 <select
                     title="list"
-                    class="text-base text-gray-800 outline-none border-2 px-4 py-2 rounded-lg"
+                    class="text-base font-extrabold text-gray-800 outline-none border-2 px-4 py-2 rounded-lg h-8"
                     prop:value={move || filter.get().to_string()}
                     on:change=move |e| {
                         let value = event_target_value(&e);
@@ -56,7 +59,7 @@ pub fn SearchBar(
                     <option value=FILTER::PID.to_string()>{FILTER::PID.to_string()}</option>
                 </select>
                 <button
-                    class="bg-indigo-600 text-white text-sm rounded-lg px-4 py-2"
+                    class="bg-indigo-600 text-white rounded-lg px-4 py-2 hover:bg-indigo-500"
                     on:click=move |_e| {
                         clear_event();
                         if let Some(input) = input_ref.get() {
@@ -64,7 +67,7 @@ pub fn SearchBar(
                         }
                     }
                 >
-                    "Clear"
+                    <img src="/public/refresh.svg" width="20" height="20" alt="Icon" />
                 </button>
             </div>
         </div>

95a72fa711de055695408e1ca12c1ecde7d0d488
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-20 17:24:05 +0900

    Fix | get_open_ports의 결과에 따라 운영체제에 맞게 파싱을 분기하였음

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,17 @@
-use utils::port;
+use enums::os::OS;
+use traits::command::OSCommandExecutor;
+use utils::port::{self, parsing_mac_lsof, parsing_window_netstat};
 
 pub mod utils {
     pub mod port;
+}
+
+pub mod traits {
+    pub mod command;
+}
+
+pub mod enums {
+    pub mod os;
 }
 
 #[tauri::command]
@@ -10,8 +20,15 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn get_open_ports() -> String {
-    port::get_open_ports()
+fn get_open_ports() -> Vec<(String, String, String)> {
+    let executor = OSCommandExecutor;
+    let os = port::get_open_ports(&executor);
+
+    match os {
+        OS::MacOS(output) => parsing_mac_lsof(&output),
+        OS::Windows(output) => parsing_window_netstat(&output),
+        _ => panic!("Unsupported OS"),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -19,6 +36,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_open_ports])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

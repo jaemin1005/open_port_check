395234205cda560d1f66cf157594a411dfa43466
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-20 20:33:08 +0900

    Feat | 분리한 모듈을을 Import

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ mod app;
 use app::*;
 use leptos::*;
 
+pub mod components {
+    pub mod table;
+    pub mod search;
+}
+
+pub mod interfaces {
+    pub mod port;
+    pub mod filter;
+}
+
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {

544c4a7a7abbfd049fe49a7ae436309c492f44c4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-20 20:01:29 +0900

    Feat | Getter 메서드 추가
    튜플로 작성된 PortInfo의 특정 필드에 접근하기 위해 사용

--- src/interfaces/port.rs
+++ src/interfaces/port.rs
@@ -1,4 +1,18 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct PortInfo(String, String, String);
+
+impl PortInfo {
+    pub fn get_process_name(&self) -> String {
+        self.0.clone()
+    }
+
+    pub fn get_port(&self) -> String {
+        self.1.clone()
+    }
+
+    pub fn get_pid(&self) -> String {
+        self.2.clone()
+    }
+}

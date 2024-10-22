240d117344f657c4eeb33eec9ee9dcad16759d36
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-22 15:47:48 +0900

    Fix | For을 통해 동적으로 테이블을 구성
    leptos의 공식문서에서 동적으로 테이블을 구성할 경우 For의 사용을 권장

--- src/components/table.rs
+++ src/components/table.rs
@@ -4,7 +4,10 @@ use leptos::*;
 use crate::interfaces::port::PortInfo;
 
 #[component]
-pub fn PortTable(props: ReadSignal<Vec<PortInfo>>) -> impl IntoView {
+pub fn PortTable(
+    props: ReadSignal<Vec<PortInfo>>,
+    delete_cb: impl Fn(String) + 'static + Clone,
+) -> impl IntoView {
     view! {
         <div class="overflow-hidden ">
         <table class=" min-w-full rounded-xl">
@@ -17,22 +20,32 @@ pub fn PortTable(props: ReadSignal<Vec<PortInfo>>) -> impl IntoView {
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-300 ">
-                {
-                    move || props.get().iter().map(|row| {
-                        view! {
+            <For
+                // a function that returns the items we're iterating over; a signal is fine
+                each=move || props.get()
+                // a unique key for each item
+                key=|prop| prop.get_pid()
+                // renders each item to a view
+                 children=move |port_info: PortInfo| {
+                        let delete_cb = delete_cb.clone();
+
+                        view!
+                        {
                             <tr class="bg-white transition-all duration-500 hover:bg-gray-100">
-                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900 ">{row.get_process_name()}</td>
-                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900">{row.get_port()}</td>
-                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900">{row.get_pid()}</td>
+                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900 ">{port_info.get_process_name()}</td>
+                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900">{port_info.get_port()}</td>
+                                <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-gray-900">{port_info.get_pid()}</td>
                                 <td class=" p-5 ">
-                                    <button class="p-2 rounded-full  group transition-all duration-500  flex item-center hover:bg-gray-600">
+                                    <button class="p-2 rounded-full  group transition-all duration-500  flex item-center hover:bg-gray-600" on:click= move |_e| {
+                                        delete_cb(port_info.get_pid());
+                                    }>
                                         <img src="/public/trash.svg" width="20" height="20" alt="Icon" />
                                     </button>
                                 </td>
                             </tr>
                         }
-                    }).collect::<Vec<_>>()
-                }
+                    }
+                />
             </tbody>
         </table>
     </div>

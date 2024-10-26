2f847ac7cb643fc11412b7d1a36981c9eec5882f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-26 15:05:29 +0900

    Feat | create_resource를 사용하기 위해 Serialize 파생 매크로 추가

--- src/interfaces/port.rs
+++ src/interfaces/port.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct PortInfo(String, String, String);
 
 impl PortInfo {
